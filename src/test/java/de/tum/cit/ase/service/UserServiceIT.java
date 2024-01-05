32db25632085baec45cf91d3daf4abece3d2cfe3
Valentin Böhm <valentin.boehm@tum.de>
2024-01-05 17:32:21 +0100

    Generated with JHipster 8.1.0

--- src/test/java/de/tum/cit/ase/service/UserServiceIT.java
+++ src/test/java/de/tum/cit/ase/service/UserServiceIT.java
@@ -1,6 +1,7 @@
 package de.tum.cit.ase.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import de.tum.cit.ase.IntegrationTest;
 import de.tum.cit.ase.domain.User;
