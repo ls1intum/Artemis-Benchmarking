0a3e3b90c7815212e5dacaf3593196ab98d3beae
Stephan Krusche <krusche@tum.de>
2026-08-21 09:52:28 +0200

    Simulation: enter a course the way the web client does (#826)

---
+++ src/main/java/de/tum/cit/aet/service/artemis/util/CourseExercisesForOverviewDTO.java
@@ -0,0 +1,16 @@
+package de.tum.cit.aet.service.artemis.util;
+
+import java.util.Set;
+
+/**
+ * Subset of the Artemis {@code GET courses/{courseId}/exercises-for-overview} response that the simulation needs.
+ *
+ * <p>This is where the participation results live after Artemis PR #12999 split the course overview per tab; they used
+ * to arrive with the whole course from the now deprecated {@code for-dashboard} endpoint.
+ *
+ * @param participationResults the results of the student's participations in this course
+ */
+public record CourseExercisesForOverviewDTO(Set<ParticipationResultDTO> participationResults) {
+
+    public record ParticipationResultDTO(Double score, Boolean rated, Long participationId) {}
+}
